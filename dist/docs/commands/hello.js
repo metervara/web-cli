d144f4780c6010f57db779f127909085d4078a03
Patrik Svensson <patrik.svensson@manyone.com>
2025-03-30 17:21:18 +0200

    fix: copy html from examples to docs

--- dist/docs/commands/hello.js
+++
@@ -1,9 +0,0 @@
-const helloCommand = {
-    name: "hello",
-    description: "Prints a friendly greeting.",
-    usage: "hello",
-    execute(args) {
-        console.log("Hello, world! This is a custom command from the host project.");
-    }
-};
-export default helloCommand;
